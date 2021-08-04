af0f938b4e6ba393516b5ec7948eda3185f35ec0
3003solutions <3003solutions@gmail.com>
2021-08-03 17:09:56 -0700

    Update if&switch.java

--- if&switch.java
+++ if&switch.java
@@ -1,5 +1,8 @@
 public class MyClass {
-    public static void main(String args[]) {
+    public static void main(String args[]) {      // if statement is more flexible because we can test different values.
+                                                  // each time we do a test we dont have to use the same test criteria
+                                                  // we can do (if value == 1 else cat = 5) use different values.
+                                                  // in switch statement we only test one value and use different cases for the same value.
         
         int value = 1;                   // if Statement
         
